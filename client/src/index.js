da3e4beebc77b029830987d04971fa247cef0b36
Adela Darmansyah <adarmansyah@microsoft.com>
2021-12-09 12:59:16 -0800

    Initial setup of Apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+// Every instance of ApolloClient uses an in-memory cache.
+// This enables it to store and reuse query results so it doesn't have to make as many network requests.
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+// But how do make the ApolloClient available to use in our react app?
+// This is where the ApolloProvider comes in.
+// ApolloProvider component uses React's Context API to make our configured
+// ApolloClient instance we made above to be available throughout a React component tree.
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
